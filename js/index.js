e1ec7e0d59b66e5aea04d2f8f29f34668e8b077a
luiz13antonio <luizantonio.ce2012@gmail.com>
2024-08-19 12:12:27 -0300

    continuaco da atividade

--- js/index.js
+++ js/index.js
@@ -4,10 +4,11 @@ function novaTarefa() {
     criar.classList.add("active");
 }
 
-function confirmarExclusao() {
+function confirmarExclusao(id) {
     overlay.classList.add("active")
     modal.classList.add("active");
     confirmacao.classList.add("active");
+    deleteID = id
 }
 
 function fecharModal() {
@@ -15,6 +16,7 @@ function fecharModal() {
     modal.classList.remove("active");
     confirmacao.classList.remove("active");
     criar.classList.remove("active")
+
 }
 
 document.addEventListener("keyup", (evento) => {
@@ -25,26 +27,34 @@ document.addEventListener("keyup", (evento) => {
 
 let tarefas = [
     {
+        id: 1,
         titulo: "pendurar quadro",
         descricao: "furrardois furos na parede para colocar as buchas e pendurar o quadro",
     },
     {
+        id: 2,
         titulo: "comer",
         descricao: "Em casa",
     },
      {
+        id: 3,
         titulo: "Reunião com clientes",
         descricao: "Ir na KF veiculos se reunir com fabricio",
     }, 
     {
+        id: 4,
         titulo: "Estudar",
         descricao: "Entender melhor Variaveis e Constantes",
     },
      {
+        id: 5,
         titulo: "Comprar capacete",
         descricao: "Ir na loja e comprar o capacete que EU ja escolhi e nao ceder ao consumismo",
     },
 ]
+
+let deleteID = 0;
+
 function listarTarefas(){
     tarefas.map((tarefa) => {
         lista.innerHTML += `
@@ -71,6 +81,18 @@ function adicionarTarefa(){
         descricao: descricao.value
     }
     tarefas.push(tarefa)
+    fecharModal();
+    listarTarefas();
+}
+
+function deletarTarefas(){
+    let tarefasFiltradas = [];
+    tarefasFiltradas = tarefas.filter(tarefa=>{
+        if(tarefa.id != deleteID){
+            return tarefa;
+        }
+    });
+    tarefas = tarefasFiltradas;
     fecharModal();
     listarTarefas();
 }
